5d083007f516b29c3ff467b020c1e1bd631341d5
nehalem <miroslav.mamrak@seznam.cz>
2023-05-24 00:37:55 +0200

    refactor: get Lombok back

--- integration-service-module/src/main/java/cz/mamrakm/integrationservice/models/AccountModel.java
+++ integration-service-module/src/main/java/cz/mamrakm/integrationservice/models/AccountModel.java
@@ -2,52 +2,21 @@ package cz.mamrakm.integrationservice.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.Date;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@EqualsAndHashCode(callSuper = true)
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
 public class AccountModel extends IntegrationApiModel {
   private Long id;
   private String firstname;
   private String lastname;
   @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private Date updatedAt;
-
-  public AccountModel() {}
-
-  public AccountModel(Long id, String firstname, String lastname, Date updatedAt) {
-    this.id = id;
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.updatedAt = updatedAt;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getFirstname() {
-    return firstname;
-  }
-
-  public void setFirstname(String firstname) {
-    this.firstname = firstname;
-  }
-
-  public String getLastname() {
-    return lastname;
-  }
-
-  public void setLastname(String lastname) {
-    this.lastname = lastname;
-  }
-
-  public Date getUpdatedAt() {
-    return updatedAt;
-  }
-
-  public void setUpdatedAt(Date updatedAt) {
-    this.updatedAt = updatedAt;
-  }
 }
--- integration-service-module/src/main/java/cz/mamrakm/integrationservice/models/CommonModel.java
+++ integration-service-module/src/main/java/cz/mamrakm/integrationservice/models/CommonModel.java
@@ -1,33 +1,21 @@
 package cz.mamrakm.integrationservice.models;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 import java.io.Serializable;
 
+@Data
+@RequiredArgsConstructor
+@ToString
 public class CommonModel implements Serializable {
 
   private String operation;
 
   private IntegrationApiModel apiModel;
 
-  public CommonModel() {}
-
-  public CommonModel(String operation, IntegrationApiModel model) {
-    this.operation = operation;
-    this.apiModel = model;
-  }
-
-  public IntegrationApiModel getApiModel() {
-    return apiModel;
-  }
-
   public void setApiModel(AccountModel apiModel) {
     this.apiModel = apiModel;
-  }
-
-  public String getOperation() {
-    return operation;
-  }
-
-  public void setOperation(String operation) {
-    this.operation = operation;
   }
 }
