25ed557f8bf33bbd16897ec4536534a15038dd03
nehalem <miroslav.mamrak@seznam.cz>
2023-05-31 23:41:57 +0200

    feat: add BillingModel.java class

---
+++ integration-service-module/src/main/java/cz/mamrakm/integrationservice/models/BillingAccountDTO.java
@@ -0,0 +1,16 @@
+package cz.mamrakm.integrationservice.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.sql.Date;
+import lombok.Data;
+
+@Data
+public class BillingAccountDTO {
+
+  private Long id;
+
+  private String name; /* firstname + lastname */
+
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  private Date lastUpdate;
+}
